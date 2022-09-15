b681e5da79140c8b61584db3d9be76e2562a79fd
Logan Donley <logan.donley@gmail.com>
2022-09-14 21:52:49 -0400

    Adding new registration form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,14 +1,18 @@
-import React from "react"
-import OldRegistrationForm from "../components/forms/registration/OldRegistrationForm"
-import NewRegistrationForm from "../components/forms/registration/NewRegistrationForm"
-import { flags } from "../lib/flags"
+import React from "react";
+import OldRegistrationForm from "../components/forms/registration/OldRegistrationForm";
+import NewRegistrationForm from "../components/forms/registration/NewRegistrationForm";
+import { flags } from "../lib/flags";
 
 const Login = () => {
   return (
     <div>
-      <OldRegistrationForm />
+      {flags.newQuotePage.isEnabled() ? (
+        <NewRegistrationForm />
+      ) : (
+        <OldRegistrationForm />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
